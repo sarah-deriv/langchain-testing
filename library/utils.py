acfd56dc7faaeca9bf8463f00516dec1682c1130
sarah-deriv <sarah.aziziyan@deriv.com>
2025-01-02 19:03:43 +0800

    add the chatbot - WIP

--- library/utils.py
+++ library/utils.py
@@ -6,7 +6,11 @@ from dotenv import load_dotenv, find_dotenv
 def load_environment():
     """Load environment variables from .env file."""
     _ = load_dotenv(find_dotenv())
+    os.environ["LANGCHAIN_TRACING_V2"] = "true"
+    os.environ["LANGCHAIN_ENDPOINT"] = "https://api.langchain.plus"
     return {
         'openai_api_key': os.getenv('OPENAI_API_KEY'),
         # Add other environment variables as needed
+        'langchain_api_key' : os.getenv('LANGCHAIN_API_KEY')
+        
     }
